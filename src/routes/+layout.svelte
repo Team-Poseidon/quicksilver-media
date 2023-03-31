ff806e7587a7e406ce6ab084487e126918f3dd67
Jason Gutierrez <jasong84@gmail.com>
2023-03-31 18:28:44 -0500

    Basic Landing Page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.postcss';
+</script>
+
+<div class="bg-slate-800">
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>Quick Silver Media</title>
+</svelte:head>
+
+<section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-md text-center lg:py-16 lg:px-12">
+        <h1 class="mb-4 text-4xl font-bold tracking-tight leading-none text-gray-900 lg:mb-6 md:text-5xl xl:text-6xl dark:text-white">Quick Silver Media</h1>
+        <p class="font-light text-gray-500 md:text-lg xl:text-xl dark:text-gray-400">Digtal Records Management.</p>
+    </div>
+</section>
